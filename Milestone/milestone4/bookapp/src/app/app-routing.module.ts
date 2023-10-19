01256dfba5de3a556a35fe6ba8d91db68af9f855
Eric-Johnston <eric.mjohnston@protonmail.com>
2023-10-19 00:41:17 -0400

    milestone4 data displaying

--- Milestone/milestone4/bookapp/src/app/app-routing.module.ts
+++ Milestone/milestone4/bookapp/src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
 
   { path: 'create', component: CreateBookComponent },
 
-  { path: 'list-cars', component: ListBooksComponent },
+  { path: 'list-books', component: ListBooksComponent },
 
   { path: 'display/:id', component: DisplayBooksComponent },
 
